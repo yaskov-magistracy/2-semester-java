f11bf45987e79392d5266a5f907d0900512a344d
yaskov <yaskovanton333@gmail.com>
2026-02-24 01:04:42 +0500

    task1493

---
+++ EduCourse/src/timus/task1493.java
@@ -0,0 +1,34 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1493 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+
+        String result;
+        if (isLucky(n - 1) || isLucky(n + 1)) {
+            result = "Yes";
+        } else {
+            result = "No";
+        }
+
+        out.println(result);
+        out.flush();
+    }
+
+    private static boolean isLucky(int n) {
+        int firstThreeDigits = n / 1000;
+        int secondThreeDigits = n % 1000;
+
+        return sumDigits(firstThreeDigits) == sumDigits(secondThreeDigits);
+    }
+
+    private static int sumDigits(int n) {
+        return (n / 100) + (n / 10 % 10) + (n % 10);
+    }
+}
