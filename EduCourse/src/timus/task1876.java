e25ed4fa0b58e1623af5a40a22bdb59fd7b75fa3
yaskov <yaskovanton333@gmail.com>
2026-02-24 23:14:45 +0500

    task1876

---
+++ EduCourse/src/timus/task1876.java
@@ -0,0 +1,22 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1876 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        // все b правых тапочек вытягиваются до 40-го левого (Фаза 2 пропускается).
+        // в первой фазе вытягивается 39 правых, а во второй — все оставшиеся (a - 40) левых.
+        int scenario1 = 2 * b +40;
+        int scenario2 = 2 * a + 39;
+
+        out.println(Math.max(scenario1, scenario2));
+        out.flush();
+    }
+}
