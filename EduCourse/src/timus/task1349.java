03d3db1ce43dba5ca2283ec83fc71eb0e407740a
yaskov <yaskovanton333@gmail.com>
2026-02-25 01:26:48 +0500

    task1349

---
+++ EduCourse/src/timus/task1349.java
@@ -0,0 +1,25 @@
+package timus;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class task1349 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+
+        String res;
+        if (n == 1) {
+            res = "1 2 3";
+        } else if (n == 2) {
+            res = "3 4 5";
+        } else {
+            res = "-1";
+        }
+
+        out.println(res);
+        out.flush();
+    }
+}
